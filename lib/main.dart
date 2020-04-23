9248d5fe959b7c15fc7762afe1971d08bc2dd4aa
harshithva <harshith11032001@gmail.com>
2020-04-23 12:32:33 +0530

    Container, column and row

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,35 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          // verticalDirection: VerticalDirection.up,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: 310,
+              color: Colors.white,
+              child: Text("conatiner 1"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+              child: Text("conatiner 2"),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.green,
+              child: Text("conatiner 3"),
+            ),
+          ],
+        )),
       ),
     );
   }
